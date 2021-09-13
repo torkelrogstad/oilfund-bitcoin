337665b60ea98be66f3a43a1cb6dd168ccea7bf5
Torkel Rogstad <torkel@rogstad.io>
2021-09-13 07:35:24 -0600

    update MSTR bitcoin count

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const norwegianCount = 5_402_171;
 const closingQ12021BTCUSD = 58_763;
 
 const bitcoinCount: { [key in Company]: number } = {
-  MSTR: 108_992,
+  MSTR: 114_042,
   SQ: 8_027,
   TSLA: 38_202,
   SEETEE: 1_170,
